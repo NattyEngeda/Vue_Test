3efc90dc863ab374118af2a10b11c3ff8a4e4a6c
Natty Engeda <nattynengeda@gmail.com>
2023-03-27 09:11:05 +0300

    Commit Now

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from "vue";
+const message = "Hello World";
+const state = reactive({
+  seen: true,
+});
+
+function changeSeen() {
+  seen = !seen;
+}
+console.log(seen);
+
+var url = "https://www.facebook.com";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{ message.split("").reverse().join("") }}</h1>
+  <button @click="changeSeen">Click Me</button>
+  <br />
+  <h1 v-if="seen">Seen</h1>
+  <a :href="url" target="_blank">Google.com</a>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#id {
+  color: red;
 }
 </style>
